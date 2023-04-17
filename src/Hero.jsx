2c3168378d658759ecbed2609f093875a69dc8ec
dselmer <122752508+dselmer@users.noreply.github.com>
2023-04-17 17:11:20 -0500

    added image structure.

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,13 +4,29 @@ import Image from "./assets/airbnb hero.jpg";
 export default function Hero() {
   return (
     <div className="heroContainer">
-      <img className="heroImage" src={Image} width="40%" />
+      <div>
+        <div>
+          <img className="heroImage" src={Image}  />
+        </div>
+        <div>
+          <img />
+        </div>
+        <div>
+          <img />
+        </div>
+        <div>
+          <img />
+        </div>
+        <div>
+          <img />
+        </div>
+      </div>
       <div className="textContent">
-      <h1>Online Experiences</h1>
-      <p>
-        Join unique interactive activities led by one-of-a-kind hosts—all
-        without leaving home.
-      </p>
+        <h1>Online Experiences</h1>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
       </div>
     </div>
   );
